662c7174179b565b4e894c2b14046ab62d45bd11
Eshaan Bahuguna <eshaanbahuguna.official@gmail.com>
2023-03-06 10:59:42 +0530

    Restructered the markup and finalized 2/3rd page

--- Client/src/components/AboutPage.jsx
+++ Client/src/components/AboutPage.jsx
@@ -33,68 +33,70 @@ export default function AboutPage(){
             
             <div id='aboutus-wrapper'>
                 <div>
-                <img id='aboutus' src={require('../assets/images/About-us.jpg')} alt="aboutus" width="420" height="600" 
-                    />
+                    <img src={require('../assets/images/About-us.jpg')} alt="aboutus" width="420" height="600" style={{borderRadius: '10px'}}/>
                 </div>
                 <div>
-                <img id='aboutus2' src={require('../assets/images/aboutus-2.jpg')} alt="aboutus" width="400" height="300" 
-                    />
-                <img id='aboutus' src={require('../assets/images/aboutus-3.jpg')} alt="aboutus" width="400" height="490" 
-                    />
+                    <img  src={require('../assets/images/aboutus-2.jpg')} alt="aboutus" width="400" height="300" style={{borderRadius: '10px', marginTop: '5rem'}} />
+                    <img src={require('../assets/images/aboutus-3.jpg')} alt="aboutus" width="400" height="490" style={{borderRadius: '10px', marginTop: '2rem'}}/>
                 </div>
                 <div>
-                <h1 style={{marginTop: '12.5rem',marginLeft: '25.5rem',color:"#ff9f0d"}}>About us </h1>
-            <p id='aboutushead' style={{marginTop: '5.5rem', marginLeft: '20.5rem'}}>“A to Z of a healthy diet to help you tackle the complexities of science all day long. <br></br> <br />
-            Cakes, Homemade Cookies, Range of Pastas, and much more now available at <br /> VIT Bhopal Food Court at Bhopal, Madhya Pradesh.” </p>
-            <button id='showmore'>Show more</button>
+                    <h1 style={{marginTop: '12.5rem',marginLeft: '25.5rem',color:"#ff9f0d"}}>About us </h1>
+                    <p style={{marginTop: '2rem', marginLeft: '20.5rem', fontSize: '1.5rem'}}>“A to Z of a healthy diet to help you tackle the complexities of science all day long. <br></br> <br />
+                    Cakes, Homemade Cookies, Range of Pastas, and much more now available at <br /> VIT Bhopal Food Court at Bhopal, Madhya Pradesh.” </p>
+                    <button id='showmore'>Show more</button>
                 </div>
-            
             </div>
-           
-            
+
+            <div id='choose-us-wrapper'>
+                <h1 id='choose-us-heading' style={{}}>Why Choose Us </h1>
+                <p style={{marginTop: '2rem', padding: '0 10rem'}}>
+                    Quality of Service, Food, Ambience and Value of Money are the primary elements for choosing a food venture. Underbelly Express is one of the most exquisite food venture in the campus of VIT Bhopal with a captivating view of  nature, perfect ambience and scrumptious food.
+                </p>
+                <p>
+                    Our team is always looking forward to provide you exceptional services and win your hearts out.
+                </p>
+            </div>
     
-            <h1 id='chooseus' style={{marginTop: '6.5rem', alignItems:'center',color:"#ff9f0d"}}>Why Choose Us </h1>
-            <p id='aboutushead2' style={{marginTop: '5.5rem', alignItems:'justify'}}>Quality of Service, Food, Ambience and Value of Money are the primary elements for choosing a food venture. <br /> Underbelly Express is one of the most exquisite food venture in the campus of VIT Bhopal with a captivating view of  nature, perfect ambience and scrumptious food.
-            Our team is always looking forward to provide you exceptional services and win your hearts out.</p>
 
+            <img  src={require('../assets/images/Why-choose-us.jpg')} alt="image cannot be loaded" style={{width: '100%'}} />
 
-            <img id='aboutus' src={require('../assets/images/Why-choose-us.jpg')} alt="aboutus" width="2000px" height="500px" 
-                    />
+            <ul className='row-alignment icons' style={{background: '#1e1e1e', width: '100%'}}>
+                <li className='column-alignment'> 
+                    <i class="fa-solid fa-graduation-cap fa-3x"></i>
+                    <p>Best Chefs</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nulla ante, varius consectetur convallis vel, aliquam at arcu.</p>
+                </li>
+                <li className='column-alignment'>  
+                    <i class="fa-solid fa-mug-hot fa-3x" ></i> 
+                    <p>120+ Item food</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nulla ante, varius consectetur convallis vel, aliquam at arcu.</p>
+                </li>
+                <li className='column-alignment'> 
+                    <i class="fa-solid fa-hands-bubbles fa-3x"></i>  
+                    <p>Clean Environment</p>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nulla ante, varius consectetur convallis vel, aliquam at arcu.</p>
+                </li>
 
+            </ul>
+            
+            <div>
+                <div id='overlay'>
+                    <img id='meetourdev'  src={require('../assets/images/meet.jpeg')} alt="meet" width="2300" height="700" />
+                </div>
+                <ul className='row-alignment'>
+                    <li id='pic'> <img id='pic' src={require('../assets/images/eshaan.jpeg')} alt="eshaan" width="380" height="450" />
+                    <div id='polaroid'> <b>Eshaan Bahuguna</b>  <br /> Read more </div></li>
 
-                    <ul className='row-alignment'>
-                        <li id='icons'> <SchoolIcon /> <br /> Best Chefs</li>
-                        <li id='icons'> <CoffeeIcon /> <br /> 120+ Item food</li>
-                        <li id='icons'> <CleanHandsIcon /> <br /> Clean Environment</li>
+                    <li id='pic'> <img id='pic' src={require('../assets/images/Asmi.jpg')} alt="eshaan" width="380" height="450" />
+                    <div id='polaroid'> <b>Asmi Bhardwaj</b>  <br /> Read more</div></li>
 
-                    </ul>
-                    <div>
-                    <div id='overlay'>
-                    <img id='meetourdev'  src={require('../assets/images/meet.jpeg')} alt="meet" width="2300" height="700" 
-                    />
-                    
-                    </div>
-                    <ul className='row-alignment'>
-                 
-                 <li id='pic'> <img id='pic' src={require('../assets/images/eshaan.jpeg')} alt="eshaan" width="380" height="450" 
-                 />
-                 <div id='polaroid'> <b>Eshaan Bahuguna</b>  <br /> Read more </div></li>
+                    <li id='pic'> <img id='pic' src={require('../assets/images/Parth.jpg')} alt="eshaan" width="380" height="450" />
+                    <div id='polaroid'> <b>Parth Dubey</b>  <br /> Read more</div></li>
 
-                 <li id='pic'> <img id='pic' src={require('../assets/images/Asmi.jpg')} alt="eshaan" width="380" height="450" 
-                 />
-                 <div id='polaroid'> <b>Asmi Bhardwaj</b>  <br /> Read more</div></li>
-
-                 <li id='pic'> <img id='pic' src={require('../assets/images/Parth.jpg')} alt="eshaan" width="380" height="450" 
-                 />
-                 <div id='polaroid'> <b>Parth Dubey</b>  <br /> Read more</div></li>
-
-                 <li id='pic'> <img id='pic' src={require('../assets/images/nabiha.jpeg')} alt="eshaan" width="380" height="450" 
-                 />
-                 <div id='polaroid'> <b>  Nabiha Khan </b> <br /> Read more</div></li>
-                 
-              
-                 </ul>
-                    </div>
+                    <li id='pic'> <img id='pic' src={require('../assets/images/nabiha.jpeg')} alt="eshaan" width="380" height="450" />
+                    <div id='polaroid'> <b>  Nabiha Khan </b> <br /> Read more</div></li>
+                </ul>
+            </div>
                     
      
                  
